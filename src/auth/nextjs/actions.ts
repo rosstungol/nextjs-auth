1182809138c63a512f24fd2d01b7b16f83d2a795
Ross Tungol <rossjstungol@gmail.com>
2025-03-06 23:59:33 +0800

    Updates signup action

--- src/auth/nextjs/actions.ts
+++ src/auth/nextjs/actions.ts
@@ -1,25 +1,51 @@
-"use server";
+'use server'
 
-import { z } from "zod";
-import { redirect } from "next/navigation";
-import { signInSchema, signUpSchema } from "./schemas";
+import { z } from 'zod'
+import { redirect } from 'next/navigation'
+import { signInSchema, signUpSchema } from './schemas'
+import { eq } from 'drizzle-orm'
+import { UserTable } from '@/drizzle/schema'
+import { db } from '@/drizzle/db'
+import { generateSalt, hashPassword } from '../core/passwordHasher'
 
 export async function signIn(unsafeData: z.infer<typeof signInSchema>) {
-  const { success, data } = signInSchema.safeParse(unsafeData);
+	const { success, data } = signInSchema.safeParse(unsafeData)
 
-  if (!success) return "Unable to log you in";
+	if (!success) return 'Unable to log you in'
 
-  redirect("/");
+	redirect('/')
 }
 
 export async function signUp(unsafeData: z.infer<typeof signUpSchema>) {
-  const { success, data } = signUpSchema.safeParse(unsafeData);
+	const { success, data } = signUpSchema.safeParse(unsafeData)
 
-  if (!success) return "Unable to create account";
+	if (!success) return 'Unable to create account'
 
-  redirect("/");
+	const existingUser = await db.query.UserTable.findFirst({
+		where: eq(UserTable.email, data.email),
+	})
+
+	if (existingUser != null) return 'Account already exists for this email'
+
+	try {
+		const salt = generateSalt()
+		const hashedPassword = await hashPassword(data.password, salt)
+
+		const [user] = await db.insert(UserTable).values({
+			name: data.name,
+			email: data.email,
+			password: hashedPassword,
+			salt,
+		})
+
+		if (user == null) return 'Unable to create account'
+	} catch {
+		return 'Unable to create account'
+	}
+
+	redirect('/')
 }
 
 export async function logOut() {
-  redirect("/");
+	redirect('/')
 }
